82f3213a6eceb753c0756dee7ff051d51411d83d
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2025-06-03 20:53:30 +0100

    Initial Commit

---
+++ DependabotTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DependabotTest
+//
+//  Created by Go TAKAGI on 2025/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DependabotTest/DependabotTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependabotTestApp.swift
+//  DependabotTest
+//
+//  Created by Go TAKAGI on 2025/06/03.
+//
+
+import SwiftUI
+
+@main
+struct DependabotTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
