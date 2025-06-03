e2e587b07437dd1d740bab5d21e692d80b49158f
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2025-06-03 20:55:41 +0100

    Add package dependency

---
+++ AppDependency/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppDependency",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AppDependency",
+            targets: ["AppDependency"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-http-types", exact: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AppDependency"),
+
+    ]
+)
